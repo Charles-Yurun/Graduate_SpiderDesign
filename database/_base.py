2b104b7e4e1e859b8dbdc1ca00cc8f0bc2cc851d
zhuyurun <zhuyurun1993@gmail.com>
2016-04-14 10:32:12 +0800

    Update _base.py

--- database/_base.py
+++ database/_base.py
@@ -2,7 +2,7 @@ import leancloud
 
 
 def register_leancloud(app):
-    leancloud.init(app_id='Xmh3GP4wHdgz4jPVpaW6jQYM-gzGzoHsz',
-                   app_key='xBmSvfdpXxBkgDhjJuqqBIWW',
-                   master_key='j8VdYDUkWaKGTirQQrKsFeUC')
+    leancloud.init(app_id='',
+                   app_key='',
+                   master_key='')
     return app
